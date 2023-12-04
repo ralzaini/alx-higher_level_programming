15023e190190f414ac535147c31dfcdab8389d8f
ralzaini <rehabalzaini@gmail.com>
2023-12-04 05:10:09 -0800

    ssssssaaa

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     res = ""
     for i in range(len(my_string)):
         if my_string[i] != 'c' or my_string[i] != 'C':
-            res += i
+            res += my_string[i]
     return res

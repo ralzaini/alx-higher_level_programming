3825c380d39af99b41045dea1393d9ed938a4f9a
ralzaini <rehabalzaini@gmail.com>
2023-12-04 05:44:29 -0800

    ssssd

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in range(len(row)):
             print("{:d}".format(row[i]), end="")
-            if i is not len(row) - 1:
+            if i != len(row) - 1:
                  print(" ", end="")
         print("")

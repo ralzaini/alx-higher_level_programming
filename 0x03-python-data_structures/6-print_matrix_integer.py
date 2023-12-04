a4510a1f9654689c7c0c8166996288e78933efef
ralzaini <rehabalzaini@gmail.com>
2023-12-04 05:37:50 -0800

    ss

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
+    for row in matrix:
+        for i in range(len(row)):
+            print("{:d}".format(row[i]), end="")
+            if i is not len(row) - 1:
+                 print(" ", end="")
+        print("")

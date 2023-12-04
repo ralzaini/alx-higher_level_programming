ee382ca1cfd4aac066d886df7fc4f9c5324ff07c
ralzaini <rehabalzaini@gmail.com>
2023-12-04 08:36:47 -0800

    ss

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,13 +3,43 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * is_palindrome - is linked list a palindrome
+ * @head: pointer to head of list
+ * Return: number of nodes
  */
-int main(void)
+int is_palindrome(listint_t **head)
 {
-	listint_t *head;
+	listint_t *slow = *head, *fast = *head;
+	listint_t *next = NULL, *prev = NULL;
+	listint_t *left = NULL, *right = NULL;
 
-	return (0);
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+	/*Find the middle*/
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
+		prev = slow;
+		slow = slow->next;
+	}
+	/*Reverse the second part of the list*/
+	prev->next = NULL;
+	while (slow != NULL)
+	{
+		next = slow->next;
+		slow->next = prev;
+		prev = slow;
+		slow = next;
+	}
+	/*Find palindrome*/
+	left = *head;
+	right = prev;
+	while (left != NULL && right != NULL)
+	{
+		if (left->n != right->n)
+			return (0);
+		left = left->next;
+		right = right->next;
+	}
+	return (1);
 }

eca7f38b939c19380c3dc121d9269180c5e4cb7d
ralzaini <rehabalzaini@gmail.com>
2023-12-06 12:03:02 -0800

    aas

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,2 @@
 def square_matrix_simple(matrix=[]):
-    square_mat = [list(map(lambda x: x ** 2, row)) for row in matrix]
-    return square_mat
+    return [list(map(lambda x: x ** 2, row)) for row in matrix]

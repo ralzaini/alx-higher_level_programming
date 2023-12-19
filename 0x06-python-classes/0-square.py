30ceb3b24dfb6ca322c707c899a7b47207818075
ralzaini <rehabalzaini@gmail.com>
2023-12-19 10:13:45 -0800

    a

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""Square Module"""
+
+class square:
+    """my first class"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square module"""
+
+class square:
+    """Defines a square"""
+
+    def __init__(self, size):
+    """
+        Initilizes a Square object.
+
+        Parameters:
+            size: size of the square.
+    """
+     self.__size = size

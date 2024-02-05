80a79de63b6a017d7adfff17a0e6ccb89adf3ab8
ralzaini <rehabalzaini@gmail.com>
2024-02-05 22:11:45 +0300

    sggsh

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Look up module """
+
+def lookup(obj):
+    """Look up the object"""
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""MyList module"""
+
+class MyList(list):
+    """
+    Class the inherit from list class
+    """
+
+    def print_sorted(self):
+        """Print elements of the list"""
+        sorted_list = sorted(self)
+        print(sorted_list)

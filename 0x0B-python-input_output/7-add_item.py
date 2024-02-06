a62ad1543689afd2c9f6a26424113a5530f55241
ralzaini <rehabalzaini@gmail.com>
2024-02-06 16:01:38 +0300

    plo

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""task 7"""
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+a_list = list(sys.argv[1:])
+
+try:
+    old_data = load_from_json_file('add_ietm.json')
+except Exception:
+    old_data = []
+
+old_data.extend(a_list)
+save_to_json_file(old_data, 'add_item.json')

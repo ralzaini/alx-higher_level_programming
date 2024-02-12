fb8d369544b2273a95526f744fda2248fcdec18e
ralzaini <rehabalzaini@gmail.com>
2024-02-12 20:55:54 +0300

    ooll

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,7 +32,7 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """"""Convert a list of dictionaries to a JSON string.""""""
+        """Convert a list of dictionaries to a JSON string."""
         if list_dictionaries is None or not list_dictionaries:
             return "[]"
         return json.dumps(list_dictionaries)

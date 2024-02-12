e52043e01882cd2ea3988536c41f353a9b94833f
ralzaini <rehabalzaini@gmail.com>
2024-02-12 20:23:28 +0300

    trial

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,8 +29,7 @@ class Base:
         """Convert a list of dictionaries to a JSON string."""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):

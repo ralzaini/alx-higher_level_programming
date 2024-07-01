2301735008fe7a22633d5737b4de9b5f3d95a892
ralzaini <139781990+ralzaini@users.noreply.github.com>
2024-07-01 12:16:05 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let counter = 0;
-while (counter < arr.length) {
-  console.log(arr[counter]);
-  counter++;
-}

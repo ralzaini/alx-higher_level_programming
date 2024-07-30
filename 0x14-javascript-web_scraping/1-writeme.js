a83628f7dd4809a688fed03f989777bfbd48bee7
ralzaini <rehabalzaini@gmail.com>
2024-07-30 12:10:08 -0700

    aaw

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const args = process.argv;
+
+const fName = args[2];
+const content = args[3];
+if (fName) {
+  fs.writeFile(fName, content, 'utf-8', (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
